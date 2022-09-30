1910549ead40b291dd41608f54dc337444419569
nalivex <analiviat456@gmail.com>
2022-09-30 10:19:19 -0300

    permissoes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,7 +26,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $this->registerPolicies();
+        $this->registerPolicies();
+
+        Gate::before(fn(User $user) => $user->email === 'usuario@gmail.com' ? true : null);
 
         //
     }
